f4cd4aeb91ffce930dbab8066e14a57d0339aa31
Rohit <rohitkumararya2881@gmail.com>
2023-12-26 00:20:41 +0530

    comment added

--- script.js
+++ script.js
@@ -1,164 +1,135 @@
-const addform = document.querySelector("#additems");
+// Selecting DOM elements
+const addForm = document.querySelector("#additems");
 const search = document.querySelector(".search");
+const listItemsContainer = document.querySelector(".listitem");
+const mainFrame = document.querySelector(".mainframe");
+const taskClear = document.querySelector(".taskclear");
 
-var noofli=0;
-let stringnofli ="";
+// Initializing variables
+let numberOfItems = 0;
+let stringNumberOfItems = "";
 
+// Function to add list item
+addForm.addEventListener("submit", (event) => {
+  function addList(value) {
+    // Selecting relevant DOM elements
+    const taskList = document.querySelector(".tasklist");
+    const listItemContainer = document.querySelector(".listitem");
+    const listItem = document.createElement("li");
 
-//li creation p creation
+    // Creating a paragraph element to hold the task text
+    const paragraph = document.createElement("p");
+    const node = document.createTextNode(value);
+    paragraph.appendChild(node);
 
+    // Appending the paragraph and delete button to the list item
+    listItem.appendChild(paragraph);
+    listItem.innerHTML +=
+      '<button class="deleteButton"><i class="fa fa-trash" aria-hidden="true" class="icons" id="delete"></i></button>';
 
+    // Appending the list item to the list container
+    listItemContainer.appendChild(listItem);
 
-addform.addEventListener("submit", event => {
-    function addList(value)
-    {  
-        const TaskList = document.querySelector(".tasklist");
-        const Listitem = document.querySelector(".listitem");
-        const li = document.createElement("li");
+    // Updating the count of items
+    numberOfItems += 1;
+    console.log(numberOfItems);
 
-        const p =  document.createElement("p");
-        const node = document.createTextNode(value);
-        p.appendChild(node);
-        
-
-        li.appendChild(p);
-        li.innerHTML+='<button class="deletebutton"><i class="fa fa-trash" aria-hidden="true" class="icons" id="delete"></i></button>';
-
-        
-    
-        Listitem.appendChild(li);
-        noofli+=1;
-        console.log(noofli)
-       
-        spanmessage.innerHTML = "It has"+" "+ noofli + " " + "items";
-
-
-    }
-    event.preventDefault();
-    const filledData = addform.filledtext.value; 
-    const TaskList = document.querySelector(".tasklist");
-    const List = document.querySelector("#list");
-    addList(filledData);
-    console.log(TaskList);
-    addform.filledtext.value="";
-
-
-})
-
-
-
-
-
-
-//bottom no of items dynamically added
-
-const Listitem = document.querySelector(".listitem");
-
-
-const mainframe = document.querySelector(".mainframe");
-const taskclear = document.querySelector(".taskclear");
-
-const divmessage = document.createElement("div");
-divmessage.setAttribute("class","message")
-const spanmessage = document.createElement("span");
-
-stringnofli = String(noofli);
-console.log(`stringofli ${stringnofli}`)
-spanmessage.innerHTML = "It has"+" "+ stringnofli + " " + "items";
-const buttonclear = document.createElement("button");
-buttonclear.innerText='clear';
-buttonclear.setAttribute("class","clear");
-divmessage.appendChild(spanmessage);
-divmessage.appendChild(buttonclear)
-taskclear.appendChild(divmessage)
-
-
-
-// delete 
-const Listitems = document.querySelector(".listitem");
-Listitems.addEventListener("click",event=>
-  { 
-     console.log(event.target)
-    if(event.target.classList.contains("fa"))
-    {
-      event.target.parentElement.parentElement.remove();
-      noofli-=1;
-      spanmessage.innerHTML = "It has"+" "+ noofli + " " + "items";
-
-
-    }
+    // Updating the message displayed at the bottom
+    spanMessage.innerHTML = "It has" + " " + numberOfItems + " " + "items";
   }
-  
-  
-  )
 
-  // search bar , filter
+  // Preventing form submission and processing data
+  event.preventDefault();
+  const filledData = addForm.filledtext.value;
+  const taskList = document.querySelector(".tasklist");
+  const listItemContainer = document.querySelector(".listitem");
+  addList(filledData);
+  console.log(taskList);
+  addForm.filledtext.value = "";
+});
 
-   search.addEventListener("keyup",event=>
-  {
-    const typevalue=search.task.value.trim();
-    Filterlist(typevalue);
- 
-   })
+// Displaying the number of items dynamically
+const messageContainer = document.createElement("div");
+messageContainer.setAttribute("class", "message");
+const spanMessage = document.createElement("span");
+stringNumberOfItems = String(numberOfItems);
+console.log(`stringNumberOfItems ${stringNumberOfItems}`);
+spanMessage.innerHTML = "It has" + " " + stringNumberOfItems + " " + "items";
+const clearButton = document.createElement("button");
+clearButton.innerText = "clear";
+clearButton.setAttribute("class", "clear");
+messageContainer.appendChild(spanMessage);
+messageContainer.appendChild(clearButton);
+taskClear.appendChild(messageContainer);
 
+// Delete functionality
+listItemsContainer.addEventListener("click", (event) => {
+  console.log(event.target);
+  if (event.target.classList.contains("fa")) {
+    // Removing the list item on delete button click
+    event.target.parentElement.parentElement.remove();
 
+    // Updating the count of items
+    numberOfItems -= 1;
+    spanMessage.innerHTML = "It has" + " " + numberOfItems + " " + "items";
+  }
+});
 
-  // reset dearch bar input
-   search.addEventListener("click",event=>
- 
-   {
-    if(event.target.classList.contains("resets"))
-    {
-      search.reset()
-      const typevalue=search.task.value.trim();
-      Filterlist(typevalue);
-    }
-   })
+// Search bar and filtering
+search.addEventListener("keyup", (event) => {
+  // Getting the search input value and trimming whitespace
+  const typeValue = search.task.value.trim();
 
+  // Filtering and showing/hiding list items based on the search value
+  filterListItems(typeValue);
+});
 
-// filter list function or filtering
+// Resetting search bar input
+search.addEventListener("click", (event) => {
+  if (event.target.classList.contains("resets")) {
+    // Resetting the search input
+    search.reset();
 
- function Filterlist(value)
- {           const listitem = document.querySelector(".listitem");
-          const listitems = listitem.children;
-        const listarray= Array.from(listitems);
-        // console.log(listarray)
-        listarray.filter(lis=>
-          {  console.log(lis)
-          return !lis.textContent.includes(value)
-          }).forEach(Element=>
-            Element.classList.add("hide")
-            )
-          
-            Array.from(listitems).
-            // console.log(listarray)
-            filter(lis=>
-              {
-              return lis.textContent.includes(value)
-              }).forEach(Element=>
-                Element.classList.remove("hide")
-                )
+    // Getting the search input value after reset and trimming whitespace
+    const typeValue = search.task.value.trim();
 
- }
+    // Filtering and showing/hiding list items based on the search value
+    filterListItems(typeValue);
+  }
+});
 
+// Filter list function for filtering
+function filterListItems(value) {
+  // Selecting the list item container and its children
+  const listItemContainer = document.querySelector(".listitem");
+  const listItems = listItemContainer.children;
+  const listArray = Array.from(listItems);
 
+  // Filtering and hiding elements
+  listArray
+    .filter((listItem) => !listItem.textContent.includes(value))
+    .forEach((element) => element.classList.add("hide"));
 
- //
- const clearbutton = document.querySelector(".taskclear");
- clearbutton.addEventListener("click",event=>
-   { 
-          if(event.target.classList.contains("clear")) 
-          {
-        const listitem = document.querySelector(".listitem");
-          const listitems = listitem.children;
-        const listarray= Array.from(listitems);
-        listarray.forEach(Element=>
-          {
-            Element.remove();
-            noofli-=1;
-               spanmessage.innerHTML = "It has"+" "+ noofli + " " + "items";
-          })
-          }
-      
- 
-   })
+  // Filtering and showing elements
+  Array.from(listItems)
+    .filter((listItem) => listItem.textContent.includes(value))
+    .forEach((element) => element.classList.remove("hide"));
+}
+
+// Clear button functionality
+const clearButtonContainer = document.querySelector(".taskclear");
+clearButtonContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("clear")) {
+    // Selecting the list item container and its children
+    const listItemContainer = document.querySelector(".listitem");
+    const listItems = listItemContainer.children;
+    const listArray = Array.from(listItems);
+
+    // Removing all elements and updating the count
+    listArray.forEach((element) => {
+      element.remove();
+      numberOfItems -= 1;
+      spanMessage.innerHTML = "It has" + " " + numberOfItems + " " + "items";
+    });
+  }
+});
